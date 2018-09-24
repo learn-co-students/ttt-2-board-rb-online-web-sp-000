ea95b45f37ca67c88e4ccbb2f664c56f3b5508fa
Sam Tamburri <tamburrisam@gmail.com>
2018-09-24 21:15:09 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,23 @@
-# Define the variable board below.
+board = [" "," "," "," "," "," "," "," "," "]
+
+print board [0]
+
+print board [1]
+
+print board [2]
+
+print board [3]
+
+print board [4]
+
+print board [5]
+
+print board [6]
+
+print board [7]
+
+print board [8]
+
+print board [9]
+
+

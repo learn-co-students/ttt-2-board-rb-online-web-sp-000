e2684cabe1993f363f5f3ce87452f2bddcb2c174
Janis Lebron <janislebron@gmail.com>
2020-04-26 02:14:34 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,2 +1,9 @@
-# Define the variable board below.
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+board = [" "," "," "," "," "," "," "," "," "]
+
+def display_board(board)
+  puts "X #{board[4]}"
+  puts "O #{board[0]}"
+  puts "X #{board[3]}"
+  puts "X #{board[4]}"
+  puts "X #{board[1]}"
+end
